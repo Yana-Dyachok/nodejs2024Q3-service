42b1a5d24e0d36b24aefc2e4faee659ef0f1d4bf
Yana <annetart1994@gmail.com>
2024-11-07 21:27:19 +0200

    feat: add swagger module using and install ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistsModule } from './artists/artists.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT;
 
 @Module({
-  imports: [UserModule, ArtistsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        () => ({
+          PORT_API: parseInt(port, 10) || 4000,
+        }),
+      ],
+    }),
+    UserModule,
+    ArtistsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-const port = process.env.PORT || 4000;
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 4000;
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .addServer(`http://localhost:${port}`)
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -17,7 +24,10 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors();
   await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger Module is running on: http://localhost:${port}/doc`);
 }
 
 bootstrap();
